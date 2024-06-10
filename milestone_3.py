1852c6fcdd7735bde94651c0b7127707821a5c68
ManishKhurmi <manish_khurmi@hotmail.co.uk>
2024-06-10 16:14:38 +0100

    took out the scrap work out of milestone_3

--- milestone_3.py
+++ milestone_3.py
@@ -1,41 +1,5 @@
-# While Loop Example
-N = 3
-Sum = 0
-  
-# This loop will run forever 
-while True: 
-    Sum += N 
-    N -= 1
-      
-    # the below condition will tell 
-    # the loop to stop 
-    if N == 0: 
-        break
-          
-print(f"Sum of First 10 Numbers is {Sum}")
-
-
 ##################################################################################################################################
-# While loop to iteratively check if the guess is in the word 
-while True: 
-    
-    guess = input('Guess a Letter:')
-
-    if len(guess) == 1 and guess.isalpha() == True:
-        print('Good guess!')
-        break
-    else: 
-        print('Invalid letter. Please, enter a single alphabetical character')
-
-# Check whether the guess is in the word 
-if guess in random_word:
-    print(f'Good guess! {guess} is in the word')
-else:
-    print(f'Sorry, {guess} is not in the word')
-
-##################################################################################################################################
-
-##################################################################################################################################
+# milestone 1 & 2
 import random
 
 word_list = ['mango', 'lychee', 'banana', 'blueberry', 'apple']
@@ -48,9 +12,6 @@ print(f'This is my list of words: {word_list}')
 print(f'This is the randomly generated word: {random_word}')
 ##################################################################################################################################
 
-# Create functions to run the checks 
-random_word
-
 def check_guess(guess):
     '''
     This function checks if the guess is in the randomly generated word.
@@ -61,13 +22,12 @@ def check_guess(guess):
     else:
         print(f'Sorry, {guess} is not in the word')
 
-#test function
+# Test check_guess
 #check_guess('B')
-
 
 def ask_for_input():
     '''
-    Asks for user thier guess and prints a message on the outcome.
+    Asks for user their guess and prints a message on the outcome.
     '''
     while True: 
         guess = input('Guess a Letter:')
@@ -82,7 +42,6 @@ def ask_for_input():
     check_guess(guess)
     
 ask_for_input()
-
 
 #################################################################
 #################################################################
