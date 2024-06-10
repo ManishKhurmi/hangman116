8170153d21a8ceb6c1f3ead456c7d4ab0e37e427
ManishKhurmi <manish_khurmi@hotmail.co.uk>
2024-06-10 13:48:40 +0100

    refactored and optimised code

--- milestone_2.py
+++ milestone_2.py
@@ -3,20 +3,29 @@ import random
 word_list = ['mango', 'lychee', 'banana', 'blueberry', 'apple']
 
 # randomly generated word 
-word = random.choice(word_list)
+random_word = random.choice(word_list)
 
 # print variables
 print(f'This is my list of words: {word_list}')
-print(f'This is the randomly generated word: {word}')
+print(f'This is the randomly generated word: {random_word}')
 
 # Create user input 
-guess = input('Enter a single letter:')
+user_guess = input('Enter a single letter:')
 #guess
 
+# Testing the user input
+#user_guess = 'a' 
+
 # check if the length of the input is 1 and the input is alphabetical
-if len(guess) == 1:
-    print('Good guess!')
-if guess.isalpha() == True:
-    print('Good guess!')
-else: 
-    print('Oops! That is not a valid input')
+def check_user_guess(user_guess):
+    if len(user_guess) == 1 and user_guess.isalpha() == True:
+        print('Good guess!')
+    else: 
+        print('Oops! That is not a valid input')
+
+check_user_guess(user_guess)
+
+
+
+
+
