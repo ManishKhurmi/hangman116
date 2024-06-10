2edd63629dcb1fbec10c8dddde30800f8a98e874
ManishKhurmi <manish_khurmi@hotmail.co.uk>
2024-06-10 17:06:09 +0100

    started to create the Hangman Class

---
+++ milestone_4.py
@@ -0,0 +1,20 @@
+import milestone_2
+
+# Giving values to the parameters 
+word_list = milestone_2.word_list
+word = milestone_2.random_word
+ 
+
+word_list
+random_word
+
+class Hangman:
+
+    def __init__(self, word_list, number_of_lives = 5):
+
+        self.word = word 
+        self.guessed 
+
+#Step 3
+
+
